8c4e9ea7a375ecfc33f91b3f616bc178ce4e959e
jcode <jcodephilosophy@gmail.com>
2021-11-23 15:13:58 +0300

    Unicode task

--- Lesson1/src/Calculator.java
+++ Lesson1/src/Calculator.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     public static void main(String[] args) {
         int num1;
